999afa6dfbf5bce4be7ee4bac75b6c98cfd9fd79
raduparvulescu <147428112+raduparvulescu@users.noreply.github.com>
2025-04-07 20:10:43 +0300

    moved all string literals under constants folder

---
+++ GildedRose/Constants/ItemNames.cs
@@ -0,0 +1,10 @@
+namespace GildedRoseKata
+{
+    public static class ItemNames
+    {
+        public const string AgedBrie = "Aged Brie";
+        public const string BackstagePass = "Backstage passes to a TAFKAL80ETC concert";
+        public const string Sulfuras = "Sulfuras, Hand of Ragnaros";
+        public const string ConjuredManaCake = "Conjured Mana Cake";
+    }
+}
--- GildedRose/Factories/UpdaterFactory.cs
+++ GildedRose/Factories/UpdaterFactory.cs
@@ -6,10 +6,10 @@ namespace GildedRoseKata
         {
             return item.Name switch
             {
-                "Aged Brie" => new AgedBrieUpdater(item),
-                "Backstage passes to a TAFKAL80ETC concert" => new BackstagePassUpdater(item),
-                "Sulfuras, Hand of Ragnaros" => new SulfurasUpdater(item),
-                "Conjured Mana Cake" => new ConjuredItemUpdater(item),
+                ItemNames.AgedBrie => new AgedBrieUpdater(item),
+                ItemNames.BackstagePass => new BackstagePassUpdater(item),
+                ItemNames.Sulfuras => new SulfurasUpdater(item),
+                ItemNames.ConjuredManaCake => new ConjuredItemUpdater(item),
                 _ => new NormalItemUpdater(item),
             };
         }
