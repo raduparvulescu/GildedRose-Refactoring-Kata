8b40e8acf6bb1b27f81a8d076927a4ee6e501840
raduparvulescu <147428112+raduparvulescu@users.noreply.github.com>
2025-04-07 20:02:21 +0300

    optimized logic for conjured items

--- GildedRose/Updaters/ConjuredItemUpdater.cs
+++ GildedRose/Updaters/ConjuredItemUpdater.cs
@@ -12,14 +12,9 @@ namespace GildedRoseKata
         public void UpdateItem()
         {
             // Base degradation for normal items is 1 before the sell date,
-            // so for conjured items, it should be 2.
-            int degradation = 2;
-            // If the sell date has passed, normal items degrade by 2,
-            // so conjured items degrade by 4.
-            if (item.SellIn <= 0)
-            {
-                degradation = 4;
-            }
+            // so for conjured items, it should be 2. If the sell date has passed,
+            // normal items degrade by 2, so conjured items degrade by 4.
+            int degradation = item.SellIn <= 0 ? 4 : 2;
             // Ensure quality doesn't drop below 0.
             item.Quality = System.Math.Max(0, item.Quality - degradation);
             // Decrease sell date.
